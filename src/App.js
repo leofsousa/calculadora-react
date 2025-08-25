dd07cd07b9a408e3e8bbb7f09aa369730329701a
Leozera2k <leofsousa98@gmail.com>
2025-08-25 10:56:30 -0300

    interface calculadora

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Container , Content } from './styles';
+import { Container , Content , Row , Column} from './styles';
 import Input from './components/input';
 import Button from './components/Button';
 
@@ -7,6 +7,25 @@ const App = () => {
     <Container>
      <Content>
       <Input />
+      <Row>
+        <Button label="7"/>
+        <Button label="8"/>
+        <Button label="9"/>
+        <Button label="-"/>
+      </Row>
+      <Row>
+        <Button label="4"/>
+        <Button label="5"/>
+        <Button label="6"/>
+        <Button label="+"/>
+      </Row>
+      <Row>
+        <Button label="1"/>
+        <Button label="2"/>
+        <Button label="3"/>
+        <Button label="="/>
+      </Row>
+
      </Content>
     </Container>
   );
--- src/styles.js
+++ src/styles.js
@@ -12,6 +12,18 @@ justify-content: center;
 
 export const Content = styled.div`
     background-color: #FFFFFF;
-    width: 80%;
-    min-height: 350px;
+    width: 60%;
+`
+
+export const Row = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`
+export const Column = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
 `
